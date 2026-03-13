7d877ff793027cf461420fb4d83ce25e721216c9
Marco Penno <marco.penno@tiscali.it>
2026-03-13 23:35:12 +0100

    Add morty in makefile + start synthesis
    Added morty in makefile. It permits to pickle all rtl files in one .sv, easier to manage with yosys+slang. It can start the synthesis

---
+++ hw/asic/sky130/sky130_sram_stub.v
@@ -0,0 +1,11 @@
+(* blackbox *)
+module sky130_sram_4kbyte_1rw_32x1024_8 (
+    input         clk0,
+    input         csb0,
+    input         web0,
+    input  [ 3:0] wmask0,
+    input  [ 9:0] addr0,
+    input  [32:0] din0,
+    output [32:0] dout0
+);
+endmodule
